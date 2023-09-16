ef564cf68521becf9676290c5447623325a3bfff
Muhyiddin Syarif <82252062+YI-SYA@users.noreply.github.com>
2023-09-16 10:55:11 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -11,7 +11,7 @@ choice = st.sidebar.selectbox("Select a Page", menu)
 if choice == 'Profile':#welcome
     with st.container():
         st.title("Helloo, Welcome to My Profile")
-        st.header("my name is _Muhyiddin Syarif_ :F39F5A")
+        st.header("my name is _Muhyiddin Syarif_ :F39F5A", divider='rainbow')
         st.write("I'm Data Science progammer from Indonesia")
     st.write("---")
     with st.container():
@@ -21,8 +21,7 @@ if choice == 'Profile':#welcome
         st.write("")
     with st.container():
         st.header("List of Project")
-        st.markdown(
-        '''Happy Streamlit-ing! :balloon:'''")
+
     #contact container
     with st.container():
         st.write("---")
