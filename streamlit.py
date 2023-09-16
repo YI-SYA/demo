2d81f68cf1e7a115e593ecb2c7a6d96fd87cca6c
Muhyiddin Syarif <82252062+YI-SYA@users.noreply.github.com>
2023-09-16 15:03:51 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -15,7 +15,7 @@ if choice == 'Profile':#welcome
         st.write("I'm Data Science progammer from Indonesia")
     st.write("---")
     with st.container():
-        st.image("https://img.freepik.com/free-vector/hand-drawn-web-developers_23-2148819604.jpg?w=1060&t=st=1694832376~exp=1694832976~hmac=71108d0b0e08c88ca08d8cf38b5b6161cdbe8a0806c7faab045a81880dd61e69", use_column_width=None)
+        st.image("https://img.freepik.com/free-vector/hand-drawn-web-developers_23-2148819604.jpg?w=1060&t=st=1694832376~exp=1694832976~hmac=71108d0b0e08c88ca08d8cf38b5b6161cdbe8a0806c7faab045a81880dd61e69", use_column_width=auto)
     st.write("---")
     with st.container():
         st.write("")
