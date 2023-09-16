82be042ea8d2d0e8c636ecdc9d8a8eb86ab516d5
Muhyiddin Syarif <82252062+YI-SYA@users.noreply.github.com>
2023-09-16 08:34:40 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,7 +7,7 @@ choice = st.sidebar.selectbox("Select a Page", menu)
 
 #My Profile
 if choice == 'Profile':
-    st.header("<h1 style='font-size: 36px;'> Welcome !!!!</h1>")
+    st.header("<h1 style='font-size: 36px;'> Welcome !!!!</h1>", unsafe_allow_html=True)
 
 
 #Project
